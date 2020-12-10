a7ad945e8caf334090cec6cf94eba9c328136510
greta-sorritelli <greta.sorritelli@studenti.unicam.it>
2020-12-10 19:22:26 +0100

    mod magazziniere e cliente

--- C3 Team MGC/src/test/java/it/unicam/cs/ids/C3/TeamMGC/javaModel/ClienteTest.java
+++ C3 Team MGC/src/test/java/it/unicam/cs/ids/C3/TeamMGC/javaModel/ClienteTest.java
@@ -1,15 +1,19 @@
 package it.unicam.cs.ids.C3.TeamMGC.javaModel;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
+
+import static it.unicam.cs.ids.C3.TeamMGC.javaPercistence.DatabaseConnection.updateData;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ClienteTest {
 
-//    @BeforeAll
-//    static void preparaDB() throws SQLException {
-//        updateData("TRUNCATE `sys`.`clienti`;");
-//    }
+    @BeforeAll
+    static void clearDB() throws SQLException {
+        updateData("TRUNCATE `sys`.`clienti`;");
+    }
 
     @Test
     void creazioneCliente() {
