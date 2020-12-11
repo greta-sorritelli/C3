38391179bfb452af8c9efce18e53b0026e531251
clarissa-albanese <clarissa.albanese@studenti.unicam.it>
2020-12-11 18:05:31 +0100

    Gestori e test

--- C3 Team MGC/src/main/java/it/unicam/cs/ids/C3/TeamMGC/javaModel/GestoreCorrieri.java
+++ C3 Team MGC/src/main/java/it/unicam/cs/ids/C3/TeamMGC/javaModel/GestoreCorrieri.java
@@ -1,10 +1,30 @@
 package it.unicam.cs.ids.C3.TeamMGC.javaModel;
 
-public class GestoreCorrieri {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
-	public int getCorrieriDisponibili() {
-		// TODO - implement GestoreCorrieri.getCorrieriDisponibili
-		throw new UnsupportedOperationException();
+import static it.unicam.cs.ids.C3.TeamMGC.javaPercistence.DatabaseConnection.executeQuery;
+
+public class GestoreCorrieri {
+	/**
+	 * @return l'elenco dei Corrieri Disponibili
+	 */
+	public ArrayList<Corriere> getCorrieriDisponibili() {
+		try {
+			ArrayList<Corriere> corrieri = new ArrayList<>();
+			ResultSet rs = executeQuery("SELECT * FROM sys.corrieri WHERE (`stato` = 'DISPONIBILE' );");
+			while (rs.next()) {
+				Corriere tmp = new Corriere(rs.getInt("ID"), StatoCorriere.valueOf(rs.getString("stato")),
+						rs.getInt("capienza"));
+				corrieri.add(tmp);
+			}
+			return corrieri;
+		} catch (SQLException exception) {
+			//todo
+			exception.printStackTrace();
+		}
+		return null;
 	}
 
 	public Corriere selezionaCorriere() {
--- C3 Team MGC/src/main/java/it/unicam/cs/ids/C3/TeamMGC/javaModel/GestoreMagazzini.java
+++ C3 Team MGC/src/main/java/it/unicam/cs/ids/C3/TeamMGC/javaModel/GestoreMagazzini.java
@@ -1,5 +1,11 @@
 package it.unicam.cs.ids.C3.TeamMGC.javaModel;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import static it.unicam.cs.ids.C3.TeamMGC.javaPercistence.DatabaseConnection.executeQuery;
+
 public class GestoreMagazzini {
 
 	/**
@@ -7,13 +13,27 @@ public class GestoreMagazzini {
 	 * @param magazzino
 	 */
 	public Magazziniere getMagazziniere(PuntoPrelievo magazzino) {
-		// TODO - implement GestoreMagazzini.getMagazziniere
-		throw new UnsupportedOperationException();
+//		return magazzino.getMagazziniere();
+		return null;
 	}
 
-	public void getMagazziniDisponibili() {
-		// TODO - implement GestoreMagazzini.getMagazziniDisponibili
-		throw new UnsupportedOperationException();
+	/**
+	 * @return l'elenco dei Magazzini Disponibili
+	 */
+	public ArrayList<PuntoPrelievo> getMagazziniDisponibili() {
+		try {
+			ArrayList<PuntoPrelievo> puntoPrel = new ArrayList<>();
+			ResultSet rs = executeQuery("SELECT * FROM sys.punti_prelievo;");
+			while (rs.next()) {
+				PuntoPrelievo tmp = new PuntoPrelievo(rs.getString("nome"), rs.getString("indirizzo"));
+				puntoPrel.add(tmp);
+			}
+			return puntoPrel;
+		} catch (SQLException exception) {
+			//todo
+			exception.printStackTrace();
+		}
+		return null;
 	}
 
 }
