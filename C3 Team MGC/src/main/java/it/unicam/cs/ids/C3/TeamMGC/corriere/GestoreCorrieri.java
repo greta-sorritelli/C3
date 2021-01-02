2255c822118d6c56424c0cdf032c59e8446c093b
clarissa-albanese <clarissa.albanese@studenti.unicam.it>
2021-01-02 18:23:27 +0100

    GestoreCorrieri e test + update vpp

--- C3 Team MGC/src/main/java/it/unicam/cs/ids/C3/TeamMGC/corriere/GestoreCorrieri.java
+++ C3 Team MGC/src/main/java/it/unicam/cs/ids/C3/TeamMGC/corriere/GestoreCorrieri.java
@@ -8,28 +8,84 @@ import static it.unicam.cs.ids.C3.TeamMGC.javaPercistence.DatabaseConnection.exe
 
 public class GestoreCorrieri {
 
+    ArrayList<Corriere> corrieri = new ArrayList<>();
+
+    /**
+     * Controlla se il {@link Corriere} che si vuole creare e' gia' presente nella lista dei corrieri. Se non e' presente
+     * viene creato e aggiunto alla lista.
+     *
+     * @return Il corriere
+     * @throws SQLException
+     */
+    private Corriere addCorriere(ResultSet rs) throws SQLException {
+        for (Corriere corriere : corrieri)
+            if (corriere.getID() == rs.getInt("ID"))
+                return corriere;
+        Corriere toReturn = new Corriere(rs.getInt("ID"), rs.getString("nome"),
+                rs.getString("cognome"), rs.getBoolean("stato"),
+                rs.getInt("capienza"));
+        addCorriereToList(toReturn);
+        return toReturn;
+    }
+
+    /**
+     * Aggiunge un {@link Corriere} alla lista di corrieri.
+     *
+     * @param corriere Corriere da aggiungere
+     * @return {@code true} se il Corriere viene inserito correttamente, {@code false} altrimenti
+     */
+    private boolean addCorriereToList(Corriere corriere) {
+        if (!corrieri.contains(corriere))
+            return corrieri.add(corriere);
+        else
+            return false;
+    }
+
+    /**
+     * Ritorna il {@link Corriere} collegato all' {@code ID}.
+     *
+     * @param ID Codice Identificativo del Corriere
+     * @return Il Corriere desiderato
+     */
+    public Corriere getCorriere(int ID) {
+        try {
+            ResultSet rs = executeQuery("SELECT * FROM sys.corrieri where ID='" + ID + "' ;");
+            if (rs.next())
+                return addCorriere(rs);
+            else
+                //todo eccezione
+                throw new IllegalArgumentException();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return null;
+    }
+
     /**
      * @return l'elenco dei Corrieri Disponibili
      */
-    //TODO fare test
+    //TODO controllare se va bene il test
     public ArrayList<Corriere> getCorrieriDisponibili() {
         try {
-            ArrayList<Corriere> corrieri = new ArrayList<>();
-            ResultSet rs = executeQuery("SELECT * FROM sys.corrieri WHERE (`stato` = 'DISPONIBILE' );");
+            ArrayList<Corriere> corrieriDisponibili = new ArrayList<>();
+            ResultSet rs = executeQuery("SELECT * FROM sys.corrieri WHERE (`stato` = 'true' );");
             if (rs.next())
-                while (rs.next()) {
-                    Corriere tmp = new Corriere(rs.getInt("ID"), true, rs.getInt("capienza"));
-                    corrieri.add(tmp);
-                }
-            else {
-                ResultSet rs1 = executeQuery("SELECT * FROM sys.corrieri WHERE (`stato` = 'DISPONIBILE' );");
-                while (!rs1.next())
-                    rs1 = executeQuery("SELECT * FROM sys.corrieri WHERE (`stato` = 'DISPONIBILE' );");
+                do {
+                    Corriere tmp = new Corriere(rs.getInt("ID"), rs.getString("nome"),
+                            rs.getString("cognome"), true, rs.getInt("capienza"));
+                    corrieriDisponibili.add(tmp);
+                } while (rs.next());
 
-                Corriere tmp = new Corriere(rs.getInt("ID"), true, rs.getInt("capienza"));
-                corrieri.add(tmp);
+            else {
+                ResultSet rs1 = executeQuery("SELECT * FROM sys.corrieri WHERE (`stato` = 'true' );");
+                while (!rs1.next())
+                    rs1 = executeQuery("SELECT * FROM sys.corrieri WHERE (`stato` = 'true' );");
+
+                Corriere tmp = new Corriere(rs.getInt("ID"), rs.getString("nome"),
+                        rs.getString("cognome"), true, rs.getInt("capienza"));
+                corrieriDisponibili.add(tmp);
             }
-            return corrieri;
+            return corrieriDisponibili;
         } catch (SQLException exception) {
             //todo
             exception.printStackTrace();
@@ -37,33 +93,46 @@ public class GestoreCorrieri {
         return null;
     }
 
-    public ArrayList<String> getDettagliCorrieriDisponibili(){
-        //todo
+    /**
+     * @return ArrayList<ArrayList < String>> dei dettagli dei corrieri disponibili.
+     */
+    public ArrayList<ArrayList<String>> getDettagliCorrieriDisponibili() {
+        try {
+            ArrayList<Corriere> corrieriDisponibili = new ArrayList<>();
+            ArrayList<ArrayList<String>> dettagli = new ArrayList<>();
+            ResultSet rs = executeQuery("SELECT * FROM sys.corrieri WHERE (`stato` = 'true' );");
+            while (rs.next()) {
+                Corriere tmp = new Corriere(rs.getInt("ID"), rs.getString("nome"),
+                        rs.getString("cognome"), true, rs.getInt("capienza"));
+                corrieriDisponibili.add(tmp);
+            }
+            for (Corriere corriere : corrieriDisponibili) {
+                dettagli.add(corriere.getDettagli());
+            }
+            return dettagli;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return null;
     }
 
-    public Corriere selezionaCorriere() {
-        // TODO - implement GestoreCorrieri.selezionaCorriere
-        throw new UnsupportedOperationException();
+    /**
+     * Seleziona il {@link Corriere} desiderato.
+     *
+     * @param ID Codice Identificativo del Corriere
+     * @return Le informazioni del Corriere
+     */
+    public ArrayList<String> selezionaCorriere(int ID) {
+
+        return getCorriere(ID).getDettagli();
     }
 
-    //todo
     public void setCapienza(int IDCorriere, int capienza) {
-
+        getCorriere(IDCorriere).setCapienza(capienza);
     }
 
-    public void selezionaCorriere(int ID){
-        //todo
-    }
-
-
-    public Corriere getCorriere(int ID){
-        //todo
-        return null;
-    }
-
-    public void setDisponibilita(int IDCorriere, boolean disponibilita){
-        //todo
+    public void setDisponibilita(int IDCorriere, boolean disponibilita) {
+        getCorriere(IDCorriere).setDisponibilita(disponibilita);
     }
 
 }
--- C3 Team MGC/src/test/java/it/unicam/cs/ids/C3/TeamMGC/corriere/GestoreCorrieriTest.java
+++ C3 Team MGC/src/test/java/it/unicam/cs/ids/C3/TeamMGC/corriere/GestoreCorrieriTest.java
@@ -1,5 +1,6 @@
 package it.unicam.cs.ids.C3.TeamMGC.corriere;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -7,24 +8,78 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import static it.unicam.cs.ids.C3.TeamMGC.javaPercistence.DatabaseConnection.updateData;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class GestoreCorrieriTest {
 
     @BeforeAll
     static void preparaDB() throws SQLException {
         updateData("TRUNCATE `sys`.`corrieri`;");
-        updateData("INSERT INTO `sys`.`corrieri` (`stato`, `capienza`) VALUES ('DISPONIBILE','52');");
-        updateData("INSERT INTO `sys`.`corrieri` (`stato`, `capienza`) VALUES ('NON_DISPONIBILE','13');");
-        updateData("INSERT INTO `sys`.`corrieri` (`stato`, `capienza`) VALUES ('DISPONIBILE','16');");
+        updateData("INSERT INTO `sys`.`corrieri` (`nome`, `cognome`, `stato`, `capienza`) VALUES ('Clarissa', 'Albanese', 'true', '52');");
+        updateData("INSERT INTO `sys`.`corrieri` (`nome`, `cognome`, `stato`, `capienza`) VALUES ('Matteo', 'Rondini','false','13');");
+        updateData("INSERT INTO `sys`.`corrieri` (`nome`,`cognome`,`stato`, `capienza`) VALUES ('Greta', 'Sorritelli','true','16');");
+    }
+
+    @AfterAll
+    static void setDisponibilita() {
+        GestoreCorrieri gestoreCorrieri = new GestoreCorrieri();
+        gestoreCorrieri.setDisponibilita(1,false);
+        gestoreCorrieri.setDisponibilita(3,true);
+        assertFalse(gestoreCorrieri.getCorriere(1).getDisponibilita());
+        assertTrue(gestoreCorrieri.getCorriere(3).getDisponibilita());
+    }
+
+    @Test
+    void getCorriere() {
+        GestoreCorrieri gestoreCorrieri = new GestoreCorrieri();
+        assertEquals(52, gestoreCorrieri.getCorriere(1).getCapienza());
+        assertEquals("Matteo", gestoreCorrieri.getCorriere(2).getNome());
+        assertEquals("true", String.valueOf(gestoreCorrieri.getCorriere(3).getDisponibilita()));
+
     }
 
     @Test
     void getCorrieriDisponibili() {
         GestoreCorrieri gestoreCorrieri = new GestoreCorrieri();
+        Corriere corriere = new Corriere(2, "Matteo", "Rondini", false,13);
         ArrayList<Corriere> test = gestoreCorrieri.getCorrieriDisponibili();
-        assertEquals(test.get(0).getID(), 1);
+        assertEquals(1, test.get(0).getID());
+        assertEquals(3, test.get(1).getID());
+        assertTrue(test.get(0).getDisponibilita());
         assertTrue(test.get(1).getDisponibilita());
+        assertFalse(test.contains(corriere));
+
+    }
+
+    @Test
+    void getDettagliCorrieriDisponibili() {
+        GestoreCorrieri gestoreCorrieri = new GestoreCorrieri();
+        ArrayList<ArrayList<String>> test = gestoreCorrieri.getDettagliCorrieriDisponibili();
+        assertEquals(test.get(0).get(0),"Clarissa");
+        assertEquals(test.get(0).get(1),"Albanese");
+        assertEquals(test.get(0).get(2),"true");
+        assertEquals(test.get(0).get(3),"52");
+        assertEquals(test.get(1).get(0),"Greta");
+        assertEquals(test.get(1).get(1),"Sorritelli");
+        assertEquals(test.get(1).get(2),"true");
+        assertEquals(test.get(1).get(3),"16");
+        assertFalse(test.get(1).contains("Matteo"));
+    }
+
+    @Test
+    void selezionaCorriere() {
+        GestoreCorrieri gestoreCorrieri = new GestoreCorrieri();
+        assertEquals("Clarissa", gestoreCorrieri.selezionaCorriere(1).get(0));
+        assertEquals("true", gestoreCorrieri.selezionaCorriere(3).get(2));
+        assertEquals("13", gestoreCorrieri.selezionaCorriere(2).get(3));
+    }
+
+    @Test
+    void setCapienza() {
+        GestoreCorrieri gestoreCorrieri = new GestoreCorrieri();
+        gestoreCorrieri.setCapienza(1, 20);
+        gestoreCorrieri.setCapienza(2, 10);
+        assertEquals(20, gestoreCorrieri.getCorriere(1).getCapienza());
+        assertEquals(10, gestoreCorrieri.getCorriere(2).getCapienza());
     }
 }
