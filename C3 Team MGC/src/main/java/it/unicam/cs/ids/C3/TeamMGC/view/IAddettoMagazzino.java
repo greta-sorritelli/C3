2f7c5a9995574d218f9b0bd6cc114c9ed2848728
matteoRondyUni <56674402+matteoRondyUni@users.noreply.github.com>
2021-01-08 17:18:45 +0100

    Creazione interfaccia JavaFXController

--- C3 Team MGC/src/main/java/it/unicam/cs/ids/C3/TeamMGC/view/IAddettoMagazzino.java
+++ C3 Team MGC/src/main/java/it/unicam/cs/ids/C3/TeamMGC/view/IAddettoMagazzino.java
@@ -2,6 +2,7 @@ package it.unicam.cs.ids.C3.TeamMGC.view;
 
 import it.unicam.cs.ids.C3.TeamMGC.corriere.GestoreCorrieri;
 import it.unicam.cs.ids.C3.TeamMGC.javaFX.JavaFXAssegnaMerceCorriere;
+import it.unicam.cs.ids.C3.TeamMGC.javaFX.JavaFXController;
 import it.unicam.cs.ids.C3.TeamMGC.javaFX.JavaFXGestioneInventario;
 import it.unicam.cs.ids.C3.TeamMGC.negozio.Negozio;
 import it.unicam.cs.ids.C3.TeamMGC.ordine.GestoreOrdini;
@@ -15,7 +16,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class IAddettoMagazzino {
+public class IAddettoMagazzino implements JavaFXController {
 
     //todo
     private Negozio negozio = new Negozio(1);
@@ -23,30 +24,6 @@ public class IAddettoMagazzino {
     private final GestoreCorrieri gestoreCorrieri = new GestoreCorrieri();
     private final GestoreMagazzini gestoreMagazzini = new GestoreMagazzini();
     private final GestoreOrdini gestoreOrdini = new GestoreOrdini(negozio);
-
-    /**
-     * Apre una nuova finestra
-     *
-     * @param a Fxml path
-     * @param b Titolo della finestra.
-     */
-    @FXML
-    public void openWindow(String a, String b, Object controller) {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(a));
-            fxmlLoader.setController(controller);
-            Parent root = fxmlLoader.load();
-            Stage stage = new Stage();
-            stage.setTitle(b);
-            stage.initModality(Modality.APPLICATION_MODAL);
-            stage.setScene(new Scene(root));
-//            Image icon = new Image("/icon.png");
-//            stage.getIcons().add(icon);
-            stage.showAndWait();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     /**
      * Apre la finestra per assegnare la merce al corriere.
